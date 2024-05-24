401f4dda3f0360498fb5ac2bd140a6a823fb24c6
Tensai99 <eimtingwi@gmail.com>
2024-05-24 21:15:12 +0200

    Pushing App.js App.css Index.js Index.css Files

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import AlgorithmVisualizer from './components/AlgorithmVisualizer'; // Update import path
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Sorting Algorithm Visualizer</h1>
+      </header>
+      <main>
+        <AlgorithmVisualizer />
+      </main>
+    </div>
+  );
+}
+
+export default App;
